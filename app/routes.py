31fc1d979c0e3d59a3c77e475b1a68aa46be3163
rcmar <rcmar@sygic.com>
2020-05-10 18:57:30 +0200

    login almost finished

--- app/routes.py
+++ app/routes.py
@@ -14,12 +14,15 @@ from matplotlib.backends.backend_svg import FigureCanvasSVG
 from matplotlib.figure import Figure
 
 from flask_login import current_user, login_user
+from flask_login import login_required
+from flask_login import logout_user
 from app.models import User
 
 
 @app.route('/')
 
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Dave'}
     posts = [
@@ -41,9 +44,23 @@ def articles():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))   
     str = calc()
     return render_template('login.html', title='Sign In', form=form, result = str)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 def onehot(label):
